12538c50e8b500b09a9e97b7bf8d4d3e02767b4e
Akihiro Ono <akihiro.ase@gmail.com>
2012-09-28 01:23:19 +0900

    closes #23, #25 Migrated to jQuery and now compatible with Redmine 2.1

--- lib/redmine_ckeditor/toolbar_helper.rb
+++ lib/redmine_ckeditor/toolbar_helper.rb
@@ -64,112 +64,23 @@ module RedmineCkeditor
       @@dict[locale][item] || (locale != "en" ? button_label(item, "en") : item)
     end
 
-    def toolbar_selector
-      items = RedmineCkeditorSetting.toolbar.flatten
+    def selected_items
+      RedmineCkeditorSetting.toolbar.flatten 
+    end
 
-      left = button_names.reject {|button|
+    def left_options
+      items = selected_items
+      options_for_select(button_names.reject {|button|
         items.include?(button)
       }.map {|item|
         [button_label(item), item]
-      }
+      })
+    end
 
-      right = items.map {|item|
+    def right_options
+      options_for_select(selected_items.map {|item|
         [button_label(item), item]
-      }
-
-      button_container = <<-EOT
-        <input type="button" class="button" value="#{I18n.t(:add)} >>"
-          onclick="moveItem('left', 'right')"/><br/>
-        <input type="button" class="button" value="<< #{I18n.t(:remove)}"
-          onclick="moveItem('right', 'left')"/><br/><br/>
-        <input type="button" class="button" value="#{I18n.t(:separator)} >>"
-          onclick="addItem('-')"/><br/>
-        <input type="button" class="button" value="#{I18n.t(:line_break)} >>"
-          onclick="addItem('/')"/>
-      EOT
-      button_container = content_tag(:div, button_container.html_safe, :class => "container")
-
-      html = hidden_field_tag("settings[toolbar]", items.join(",")) +
-        content_tag(:select, options_for_select(left),
-          :id => "left", :multiple => true, :size => 10,
-          :style => "width:250px") +
-        button_container +
-        content_tag(:select, options_for_select(right),
-          :id => "right", :multiple => true, :size => 10,
-          :style => "width:250px") +
-        content_tag(:div, nil, :class => "clear") +
-        content_tag(:div, nil, :id => "toolbar")
-
-      javascript_include_tag(Redmine::Utils.relative_url_root +
-        '/plugin_assets/redmine_ckeditor/javascripts/ckeditor/ckeditor') +
-      stylesheet_link_tag(Redmine::Utils.relative_url_root +
-        '/plugin_assets/redmine_ckeditor/stylesheets/selector') +
-      content_tag(:div, html, :class => "selector-container") +
-      javascript_tag(<<-EOT)
-        function moveItem(from, to) {
-          from = $(from);
-          to = $(to);
-          var index = to.selectedIndex;
-          var i = 0;
-          while (i < from.options.length) {
-            var option = from.options[i];
-            if (!option.selected) {
-              i++;
-              continue;
-            }
-
-            option = from.removeChild(option);
-            if (option.value != '-' && option.value != '/') {
-              if (index < 0) {
-                to.appendChild(option)
-              } else {
-                to.insertBefore(option, to.options[index])
-              }
-            }
-          }
-          to.selectedIndex = -1;
-          changeHandler();
-        }
-
-        function addItem(item) {
-          var option = new Option(item, item);
-          option.innerHTML = item;
-          var to = $('right');
-          var index = to.selectedIndex;
-          if (index < 0) {
-            to.appendChild(option);
-          } else {
-            to.insertBefore(option, to.options[index])
-          }
-          changeHandler();
-        }
-        
-        function changeHandler() {
-          var values = $A($('right').children).pluck('value');
-          $('settings_toolbar').value = values.join(',');
-          
-          var bars = [];
-          var bar = [];
-          values.each(function(value) {
-            if (value == "/") {
-              bars.push(bar, value);
-              bar = [];
-            } else {
-              bar.push(value);
-            }
-          });
-          if (bar.length > 0) bars.push(bar);
-          
-          CKEDITOR.config.toolbar = bars;
-          CKEDITOR.config.language = "#{current_language.to_s.downcase}";
-          CKEDITOR.instances['toolbar'].destroy();
-          CKEDITOR.replace('toolbar');
-        }
-        
-        CKEDITOR.config.toolbar = #{RedmineCkeditorSetting.toolbar.inspect};
-        CKEDITOR.config.language = "#{current_language.to_s.downcase}";
-        CKEDITOR.replace('toolbar');
-      EOT
+      })
     end
   end
 end
