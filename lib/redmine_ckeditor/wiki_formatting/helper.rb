7eeb53ebb18b342fa916f9baa3d708ce077738a0
Akihiro Ono <akihiro.ase@gmail.com>
2012-10-20 13:32:32 +0900

    fixed #28, fixed #29

--- lib/redmine_ckeditor/wiki_formatting/helper.rb
+++ lib/redmine_ckeditor/wiki_formatting/helper.rb
@@ -3,14 +3,16 @@ module RedmineCkeditor::WikiFormatting
     def replace_editor(field_id)
       javascript_tag <<-EOT
       $(document).ready(function() {
-        CKEDITOR.config.contentsCss = "#{stylesheet_path "application"}";
-        CKEDITOR.config.bodyClass = "wiki";
-        CKEDITOR.config.toolbar = #{RedmineCkeditorSetting.toolbar.inspect};
-        CKEDITOR.config.language = "#{current_language.to_s.downcase}";
+        $(":submit").siblings("a").each(function() {
+          var a = $(this);
+          if (a.attr("onclick").indexOf("preview") >= 0) a.hide();
+        });
 
-        var textarea = $('##{field_id}');
-        textarea.parent().before($('<br/>'));
-        var editor = CKEDITOR.replace(textarea.get(0), {
+        var id = '#{field_id}';
+        var textarea = document.getElementById(id);
+        if (!textarea) return;
+
+        var editor = CKEDITOR.replace(textarea, {
           on: {
             instanceReady : function(ev) {
               var writer = this.dataProcessor.writer;
@@ -22,14 +24,24 @@ module RedmineCkeditor::WikiFormatting
             }
           }
         });
-        $(":submit").siblings("a").each(function() {
-          var a = $(this);
-          if (a.attr("onclick").indexOf("preview") >= 0) a.hide();
-        });
 
         // fire change event
-        setInterval(function(){textarea.val(editor.getData());}, 1000);
+        RedmineCkeditor.intervalId[id] =
+          setInterval(function(){ textarea.value = editor.getData(); }, 1000);
       });
+      EOT
+    end
+
+    def set_config
+      javascript_tag <<-EOT
+        CKEDITOR.config.contentsCss = "#{stylesheet_path "application"}";
+        CKEDITOR.config.bodyClass = "wiki";
+        CKEDITOR.config.toolbar = #{RedmineCkeditorSetting.toolbar.inspect};
+        CKEDITOR.config.language = "#{current_language.to_s.downcase}";
+
+        var RedmineCkeditor = {
+          intervalId: {}
+        };
       EOT
     end
 
@@ -41,12 +53,24 @@ module RedmineCkeditor::WikiFormatting
           if (focus != null) CKEDITOR.instances[focus].focus();
           $('html, body').animate({scrollTop: elem.offset().top}, 100);
         }
+
+        function destroyEditor(id) {
+          clearInterval(RedmineCkeditor.intervalId[id])
+          if (CKEDITOR.instances[id]) CKEDITOR.instances[id].destroy();
+        }
       EOT
     end
 
+    def initial_setup
+      javascript_include_tag('ckeditor/ckeditor', :plugin => 'redmine_ckeditor') + set_config + overwrite_functions
+    end
+
     def wikitoolbar_for(field_id)
-      javascript_include_tag('ckeditor/ckeditor', :plugin => 'redmine_ckeditor') +
-        replace_editor(field_id) + overwrite_functions
+      if params[:format] == "js"
+        replace_editor(field_id)
+      else
+        initial_setup + replace_editor(field_id)
+      end
     end
 
     def initial_page_content(page)
