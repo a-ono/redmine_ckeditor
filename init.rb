51c108c47f368dc7f2378655f5a35620e5c26d54
Akihiro Ono <akihiro.ase@gmail.com>
2013-03-10 20:28:56 +0900

    Fixed #37 a bug of quote button

--- init.rb
+++ init.rb
@@ -9,8 +9,8 @@ Redmine::Plugin.register :redmine_ckeditor do
   name 'Redmine CKEditor plugin'
   author 'Akihiro Ono'
   description 'This is a CKEditor plugin for Redmine'
-  version '0.2.1'
-  requires_redmine :version_or_higher => '2.1.0'
+  version '0.3.0'
+  requires_redmine :version_or_higher => '2.2.0'
   url 'http://github.com/a-ono/redmine_ckeditor'
 
   settings(:partial => 'settings/ckeditor')
