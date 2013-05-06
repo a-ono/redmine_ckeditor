f0f666359a5772d2297b0076d454743b4c121980
Akihiro Ono <akihiro.ase@gmail.com>
2013-05-06 23:18:43 +0900

    refs #21 Added support for code syntax highlighting

--- lib/redmine_ckeditor/wiki_formatting/formatter.rb
+++ lib/redmine_ckeditor/wiki_formatting/formatter.rb
@@ -5,9 +5,15 @@ module RedmineCkeditor::WikiFormatting
     end
 
     def to_html(&block)
-      ActionView::Base.white_list_sanitizer.sanitize @text,
+      ActionView::Base.white_list_sanitizer.sanitize(@text,
         :tags => RedmineCkeditor::ALLOWED_TAGS,
         :attributes => RedmineCkeditor::ALLOWED_ATTRIBUTES
+      ).gsub(/<pre>\s*<code\s+(?:class="(\w+)")>[\r\n]*([^<]*?)[\r\n]*<\/code>\s*<\/pre>/) {
+        lang, code = $~.captures
+        %Q[<pre>\n<code class="#{lang} syntaxhl">#{
+          Redmine::SyntaxHighlighting.highlight_by_language(code, lang)
+        }</code>\n</pre>]
+      }
     end
   end
 end
