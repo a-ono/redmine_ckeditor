8a1cb24f4e162f74aedd9bd45b91941a43a15e04
Akihiro Ono <akihiro@ase.co.jp>
2014-02-27 14:54:04 +0900

    refs #91 Fixed IE issues by https://github.com/bastiaanterhorst/rich/pull/86

---
+++ lib/redmine_ckeditor/rich_files_controller_patch.rb
@@ -0,0 +1,31 @@
+require_dependency 'rich/files_controller'
+
+module Rich
+  class FilesController
+    def create
+      @file = RichFile.new(:simplified_type => params[:simplified_type])
+      
+      if(params[:scoped] == 'true')
+        @file.owner_type = params[:scope_type]
+        @file.owner_id = params[:scope_id].to_i
+      end
+      
+      # use the file from Rack Raw Upload
+      file_params = params[:file] || params[:qqfile]
+      if(file_params)
+        file_params.content_type = Mime::Type.lookup_by_extension(file_params.original_filename.split('.').last.to_sym)
+        @file.rich_file = file_params
+      end
+      
+      if @file.save
+        response = { :success => true, :rich_id => @file.id }
+      else
+        response = { :success => false,
+                     :error => "Could not upload your file:\n- "+@file.errors.to_a[-1].to_s,
+                     :params => params.inspect }
+      end
+
+      render :json => response, :content_type => "text/html"
+    end
+  end
+end
