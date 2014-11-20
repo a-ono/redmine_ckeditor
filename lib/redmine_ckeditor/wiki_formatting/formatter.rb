cae6c89c7854cdef9d76c7c568ca7137090eef16
Akihiro Ono <akihiro.ase@gmail.com>
2014-11-20 22:34:06 +0900

    fixes #132 avoid escape in auto_link!

--- lib/redmine_ckeditor/wiki_formatting/formatter.rb
+++ lib/redmine_ckeditor/wiki_formatting/formatter.rb
@@ -1,6 +1,6 @@
 module RedmineCkeditor::WikiFormatting
   class Formatter
-    include Redmine::WikiFormatting::LinksHelper
+    AUTO_LINK_RE = const_defined?(:AUTO_LINK_RE) ? :AUTO_LINK_RE : Redmine::WikiFormatting::LinksHelper::AUTO_LINK_RE
 
     def initialize(text)
       @text = text
@@ -19,6 +19,27 @@ module RedmineCkeditor::WikiFormatting
       }.gsub(/\{\{(.*?)\}\}/) {
         "{{" + CGI.unescapeHTML($1) + "}}"
       }
+    end
+
+    def auto_link!(text)
+      text.gsub!(AUTO_LINK_RE) do
+        all, leading, proto, url, post = $&, $1, $2, $3, $6
+        if leading =~ /<a\s/i || leading =~ /![<>=]?/
+          # don't replace URLs that are already linked
+          # and URLs prefixed with ! !> !< != (textile images)
+          all
+        else
+          # Idea below : an URL with unbalanced parenthesis and
+          # ending by ')' is put into external parenthesis
+          if ( url[-1]==?) and ((url.count("(") - url.count(")")) < 0 ) )
+            url=url[0..-2] # discard closing parenthesis from url
+            post = ")"+post # add closing parenthesis to post
+          end
+          content = proto + url
+          href = "#{proto=="www."?"http://www.":proto}#{url}"
+          %(#{leading}<a class="external" href="#{href}">#{content}</a>#{post}).html_safe
+        end
+      end
     end
   end
 end
