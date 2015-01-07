22b1ef9fe05e8659add44a6944a46708017852ed
Akihiro Ono <akihiro.ase@gmail.com>
2015-01-08 00:46:16 +0900

    Version 1.0.17

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_ckeditor do
   name 'Redmine CKEditor plugin'
   author 'Akihiro Ono'
   description 'This is a CKEditor plugin for Redmine'
-  version '1.0.16'
+  version '1.0.17'
   requires_redmine :version_or_higher => '2.3.0'
   url 'http://github.com/a-ono/redmine_ckeditor'
 
