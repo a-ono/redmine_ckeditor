09d0c3da4a38101fb3eade7640f505a00444637a
Luckyvb <vb@igray.ru>
2015-07-09 13:46:08 +0300

    Update pdf_patch.rb
    Fix images with src as base64 string on export to PDF

--- lib/redmine_ckeditor/pdf_patch.rb
+++ lib/redmine_ckeditor/pdf_patch.rb
@@ -5,6 +5,8 @@ module RedmineCkeditor
     def self.included(base)
       base.class_eval do
         alias_method_chain :formatted_text, :ckeditor
+        alias_method_chain :getImageFilename, :ckeditor
+        alias_method_chain :RDMwriteHTMLCell, :ckeditor
       end
     end
 
@@ -12,6 +14,33 @@ module RedmineCkeditor
       html = formatted_text_without_ckeditor(text)
       html = HTMLEntities.new.decode(html) if RedmineCkeditor.enabled?
       html
+    end
+    
+    def RDMwriteHTMLCell_with_ckeditor(w, h, x, y, txt='', attachments=[], border=0, ln=1, fill=0)
+      @tmp_images = []
+      RDMwriteHTMLCell_without_ckeditor(w, h, x, y, txt, attachments, border, ln, fill)
+      @tmp_images.each do |item|
+        #logger.info item
+        File.delete(item) if File.file?(item)
+      end
+    end
+
+    def getImageFilename_with_ckeditor(attrname)
+      type = attrname[/^data:.+\/(.+);base64,(.*)$/,1]
+      data = attrname[/^data:.+\/(.+);base64,(.*)$/,2]
+      img = nil
+      if data && type
+        tmp = Tempfile.new([@tmp_images.length.to_s, '.'+type])
+        img = tmp.path
+        tmp.close
+        @tmp_images << img
+        f = File.open(img,'wb')
+        f.puts(Base64.decode64(data))
+        f.close
+      else
+        img = getImageFilename_without_ckeditor(attrname)
+      end
+      img
     end
   end
 
