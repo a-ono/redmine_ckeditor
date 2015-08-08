5120299268798dacc4d62dc347e9b7bd78133f92
Akihiro Ono <akihiro.ase@gmail.com>
2015-08-09 00:08:29 +0900

    refs #163, refs #166 Migrate to new Rails sanitizer

--- lib/redmine_ckeditor/queries_helper_patch.rb
+++ lib/redmine_ckeditor/queries_helper_patch.rb
@@ -3,7 +3,7 @@ require_dependency 'queries_helper'
 module QueriesHelper
   def csv_value_with_ckeditor(column, issue, value)
     if RedmineCkeditor.enabled? && column.name == :description
-      text = HTML::FullSanitizer.new.sanitize(value.to_s)
+      text = Rails::Html::FullSanitizer.new.sanitize(value.to_s)
       text.gsub(/(?:\r\n\t*)+/, "\r").gsub("&nbsp;", " ").strip
     else
       csv_value_without_ckeditor(column, issue, value)
