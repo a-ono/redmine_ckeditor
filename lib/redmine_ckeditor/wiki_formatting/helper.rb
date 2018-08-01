1002a52b68347750b0f3ca91f135024be970ae20
Robert Beckett <bbeckett@netvu.org.uk>
2018-08-01 14:49:40 +0000

    Add onbeforeunload handler to warn on leaving dirty pages
    Signed-off-by: Robert Beckett <bbeckett@netvu.org.uk>

--- lib/redmine_ckeditor/wiki_formatting/helper.rb
+++ lib/redmine_ckeditor/wiki_formatting/helper.rb
@@ -17,6 +17,13 @@ module RedmineCkeditor::WikiFormatting
 
         var editor = CKEDITOR.replace(textarea, #{RedmineCkeditor.options(@project).to_json});
         editor.on("change", function() { textarea.value = editor.getSnapshot(); });
+	$(window).on("beforeunload",
+		function() {
+			if (editor.checkDirty()) {
+				return "Unsaved changes!";
+			}
+		}
+	);
       })();
       EOT
     end
